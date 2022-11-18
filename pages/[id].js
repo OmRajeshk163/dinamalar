661bd4633b59d37e798c3b2870a94b688a82a602
OmRajeshk163 <105838703+OmRajeshk163@users.noreply.github.com>
2022-11-18 11:55:56 +0530

    Progress bar added

--- pages/[id].js
+++ pages/[id].js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import FeedList from "../components/modules/home/FeedList";
 import styles from "../styles/Home.module.css";
 import axios from "axios";
 import { baseUrl } from "../components/modules/constants";
+import ProgressBar from "../components/elements/progressbar";
 
 const Main = ({ newsFeeds }) => {
-  const router = useRouter();
-  const mainId = router.query.id;
+  const [progress, setProgress] = useState({ value: 0, show: true });
+  useEffect(() => {
+    let interval = null;
+
+    if (newsFeeds.status !== 200) {
+      if (progress.value >= 100) {
+        clearInterval(interval);
+      } else if (progress.value < 100) {
+        interval = setInterval(() => {
+          setProgress((prev) => ({
+            ...prev,
+            value: prev.value < 80 ? prev.value + 10 : 80,
+          }));
+        }, 1000);
+      }
+    } else if (newsFeeds.status === 200) {
+      setProgress((prev) => ({ ...prev, value: 100 }));
+      setTimeout(() => setProgress((prev) => ({ ...prev, show: false })), 2000);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [newsFeeds, progress.value]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -20,7 +43,10 @@ const Main = ({ newsFeeds }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <FeedList mainId={mainId} newsFeeds={newsFeeds} />
+        {progress.show && (
+          <ProgressBar width={progress.value} height="0.45rem" />
+        )}
+        <FeedList newsFeeds={newsFeeds} />
       </main>
     </div>
   );
